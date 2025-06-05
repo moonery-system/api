991f8097b3de516dee31ab9f90d824717ae139ee
sena <passosjoaogabriel29@gmail.com>
2025-06-04 23:16:15 -0300

    [ backend ] feat: User, Permission, Role Models

---
+++ backend/app/Models/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $table = 'roles';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function users(){
+        return $this->belongsToMany(User::class, 'user_roles');
+    }
+
+    public function permissions(){
+        return $this->belongsToMany(Permission::class,'role_permissions');
+    }
+}
