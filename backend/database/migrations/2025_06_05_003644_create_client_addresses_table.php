4a4ec0d001b58d7a63483fd93f5dad82cb8cc640
sena <passosjoaogabriel29@gmail.com>
2025-06-04 21:39:10 -0300

    [ backend ] feat: client addresses migration

---
+++ backend/database/migrations/2025_06_05_003644_create_client_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('address_line');
+            $table->string('neighborhood');
+            $table->string('city', 150);
+            $table->string('state', 100);
+            $table->string('zip_code', 50);
+            $table->string('complement', 100)->nullable();
+            $table->boolean('is_primary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_addresses');
+    }
+};
