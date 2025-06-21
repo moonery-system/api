1fa2721b4ba17ccbf5feddfcf58b7b5a80816820
js3na <passosjoaogabriel29@gmail.com>
2025-06-21 01:15:41 -0300

    [ backend ] feat: LogRepository

--- backend/app/Services/LogService.php
+++ backend/app/Services/LogService.php
@@ -4,14 +4,19 @@ namespace App\Services;
 
 use App\Enums\LogEventTypeEnum;
 use App\Models\Log;
+use App\Repositories\LogRepository;
 
 class LogService
 {
-    public function record(LogEventTypeEnum $eventType, $context = [])
+    public function __construct(
+        private LogRepository $logRepository
+    ){}
+
+    public function record(LogEventTypeEnum $eventType, $context = []): void
     {
         $user = auth()->id();
 
-        Log::create([
+        $this->logRepository->create([
             'user_id' => $user,
             'event_type' => $eventType->value,
             'context' =>  $context,
